ad76ea7360af3a9205987647d0af01e7e1deeb1d
devRenatoRodrigues <rehrlz@hotmail.com>
2022-10-20 23:31:57 -0300

    Operadores logicos

---
+++ trybe-exercicios/fundamentos/introducao-a-javascript/dia-1-javascript-primeiros-passos/condIfElse.js
@@ -0,0 +1,12 @@
+const notaDesafio = 13;
+
+if (notaDesafio >= 80) {
+    console.log ("Parabéns, você foi aprovada(o)");
+}
+else if (notaDesafio < 80 && notaDesafio >= 60) {
+    console.log ("Você está na nossa lista de espera");
+} 
+else if (notaDesafio < 60 )
+{
+    console.log ("Você foi reprovada(o)");
+}
