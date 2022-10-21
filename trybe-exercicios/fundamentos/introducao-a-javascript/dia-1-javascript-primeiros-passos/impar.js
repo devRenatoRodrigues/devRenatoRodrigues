32cc2243fa68a1e52267de9f264ef46083e55a7d
devRenatoRodrigues <rehrlz@hotmail.com>
2022-10-21 13:48:25 -0300

    exercicio Impar

---
+++ trybe-exercicios/fundamentos/introducao-a-javascript/dia-1-javascript-primeiros-passos/impar.js
@@ -0,0 +1,9 @@
+const num1 = 4;
+const num2 = 6;
+const num3 = 2;
+
+if (num1 % 2 != 0 || num2 % 2 != 0 || num3 % 2 != 0) {
+    console.log ('True, um deles é IMPAR')
+} else {
+    console.log ('Nenhum é IMPAR')
+}
