0e0c2d28d1a2bb994159d821371bc7d4216c5356
devRenatoRodrigues <rehrlz@hotmail.com>
2022-10-26 18:12:27 -0300

    exercicos Academia de logica

---
+++ trybe-exercicios/fundamentos/introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio3Acad.js
@@ -0,0 +1,10 @@
+// Exercício 3 - Crie uma função que receba uma string word e outra string ending. Verifique se a string ending é o final da string word.
+// Considere que a string ending sempre será menor que a string word.
+// Dica: Use o split.
+
+// valor de teste: 'trybe' e 'be'
+// valor esperado no retorno da função: true
+// verificaFimPalavra('trybe', 'be');
+// Retorno esperado: true
+// verificaFimPalavra('joaofernando', 'fernan');
+// Retorno esperado: false;
---
+++ trybe-exercicios/fundamentos/introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicioFuncoes.js
@@ -0,0 +1,40 @@
+//Crie uma função que receba uma string e retorne true se for um palíndromo, 
+//ou false, se não for.
+
+// function palindromo (vpalindromo) {
+//     let rPalindromo = vpalindromo.split('').reverse().join('');
+//     return rPalindromo === vpalindromo
+// }
+// console.log(palindromo('tebet'));
+
+//Crie uma função que receba um array de inteiros e 
+//retorne o índice do maior valor.
+
+//  let arr = [2, 3, 6, 75, 10, 1];
+
+// function maiorI (arr) {
+//     let maiorI = 0;
+//     for (let indice in arr) {
+//         if (arr[maiorI] < arr[indice]) {
+//             maiorI = indice;
+//         }
+//     }
+//     return maiorI;
+// }
+// console.log(maiorI(arr));
+
+// Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+
+// let arr = [2, 4, 6, -10, 10, 5, -3];
+
+// function menorI (arr) {
+//     let menorI = 0;
+//     for (let indice in arr) {
+//         if (arr[menorI] > arr[indice]) {
+//             menorI = indice;
+//         }
+//     }
+//     return menorI;
+// }
+
+// console.log(menorI(arr));
