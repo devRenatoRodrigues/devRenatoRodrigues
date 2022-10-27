84ac42718beb16debaecd7c1a757542847fd6af4
devRenatoRodrigues <rehrlz@hotmail.com>
2022-10-27 13:41:06 -0300

    exercicos longtestWord

---
+++ trybe-exercicios/fundamentos/introducao-a-javascript/dia-4-javascript-let-const-template-literals-and-arrowFunctions/exercicioPalavra.js
@@ -0,0 +1,12 @@
+//Crie uma função que receba uma frase como parâmetro e retorne a maior palavra da frase.
+
+// longestWord('Antônio foi no banheiro e não sabemos o que aconteceu') retorna 'aconteceu'
+
+
+const longestWord = frase => { 
+   
+    frase = frase.split (" ")
+     return frase.sort((a, b) => b.length - a.length )[0]
+    }
+
+console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'))
