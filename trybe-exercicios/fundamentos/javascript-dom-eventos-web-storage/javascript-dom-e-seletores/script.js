0aad504fccb22323dd406a1d1847abb2f5d939a1
devRenatoRodrigues <rehrlz@hotmail.com>
2022-10-28 19:10:06 -0300

    exercicio 1

---
+++ trybe-exercicios/fundamentos/javascript-dom-eventos-web-storage/javascript-dom-e-seletores/script.js
@@ -0,0 +1,16 @@
+document.getElementById('footer-container').style.backgroundColor = 'green'
+
+document.getElementById('header-container').style.backgroundColor = 'green'
+
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'salmon'
+
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'yellow'
+
+// document.getElementsByTagName('h3')[0].style.backgroundColor = "purple"
+
+// document.getElementsByTagName('h3')[1].style.backgroundColor = "purple"
+
+
+document.getElementsByTagName('h3')[2].style.backgroundColor = 'black'
+
+document.getElementsByTagName('h3')[3].style.backgroundColor = 'black'
