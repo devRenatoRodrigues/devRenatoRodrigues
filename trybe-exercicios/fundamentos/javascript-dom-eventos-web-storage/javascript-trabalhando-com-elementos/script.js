1ce57d5f4278436df733b5824a99e6211cc9120b
devRenatoRodrigues <rehrlz@hotmail.com>
2022-11-10 18:32:10 -0300

    requisito 11 completo

---
+++ trybe-exercicios/fundamentos/javascript-dom-eventos-web-storage/javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,5 @@
+document.getElementById("elementoOndeVoceEsta").parentNode.style.backgroundColor = "blue"
+document.getElementById("primeiroFilhoDoFilho").innerText = "Palmeiras minha vida é você"
+document.getElementById("pai").firstElementChild.innerText = "Palmeiras Campeão Quarta Feira"
+document.getElementById("elementoOndeVoceEsta").previousElementSibling
+document.getElementById("elementoOndeVoceEsta").nextElementSibling
